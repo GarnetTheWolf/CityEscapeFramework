3fff56095559c9740fb4fa4c9c9d2033e4743a15
Garnet The Wolf <garnetthewolf@gmail.com>
2024-09-18 10:48:53 -0500

    Add UE5 project files
    Created a new UE 5.4.4 third-person project as the template

---
+++ Source/CityEscapeFramework/CityEscapeFrameworkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CityEscapeFrameworkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACityEscapeFrameworkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACityEscapeFrameworkGameMode();
+};
+
+
+
